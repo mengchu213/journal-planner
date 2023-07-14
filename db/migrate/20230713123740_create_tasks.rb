0a3f229fa93d96081ace9fadaa89809bbb7e533c
mengchu213 <joseinfa1022@gmail.com>
2023-07-14 13:59:05 +0800

    task and category mvc

---
+++ db/migrate/20230713123740_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.text :description
+      t.datetime :deadline
+      t.boolean :completed, default: false
+
+      t.timestamps
+    end
+  end
+end
