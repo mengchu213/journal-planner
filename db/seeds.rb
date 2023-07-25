c51896b0eb0a555a7f2986e8166b69c4e6a5a960
mengchu213 <joseinfa1022@gmail.com>
2023-07-25 19:05:16 +0800

    admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!(name: 'jose infante', username: 'joe123', email: 'joseinfa1022@gmail.com', password: 'secret', admin: true)
